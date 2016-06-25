2da092c2b631f1b55bbf9d0eb890ffac01c57a94
Hanif Fakira <fakira.work@gmail.com>
2016-06-25 18:05:09 +0100

    US1: add spy test for airport

--- tests/lib_airport_tests.py
+++ tests/lib_airport_tests.py
@@ -12,13 +12,18 @@ class TestAirport(unittest.TestCase):
         """airport is empty"""
         self.assertEqual(self.airport.planes, [])
 
-    def test_1a_land(self):
+    def test_1a_instruct_to_land(self):
         """plane in airport after landing"""
         self.airport.instruct_to_land(self.plane)
         number_of_planes = self.airport.planes
         self.assertEqual(1, len(number_of_planes))
 
-    def test_1b_land(self):
+    def test_1b_instruct_to_land(self):
         """aiport hanger filled with one plane"""
         self.airport.instruct_to_land(self.plane)
         self.assertIn(self.plane, self.airport.planes)
+
+    def test_1c_instruct_to_land(self):
+        """aiport instruct_to_land calls land on """
+        self.airport.instruct_to_land(self.plane)
+        self.plane.land.assert_called_with()
