f8ae98a601dd9719415a07151290f6c6dec43a5b
Hanif Fakira <fakira.work@gmail.com>
2016-06-26 11:13:56 +0100

    US4: refactoring

--- lib/airport.py
+++ lib/airport.py
@@ -4,15 +4,24 @@ class Airport(object):
         self.capacity = 20
 
     def instruct_to_land(self, plane):
-        if len(self.planes) == self.capacity:
-            raise Exception('Airport is full: Take off plane')
-        if plane in self.planes:
-            raise Exception('Plane already at airport')
+        self.__is_airport_full()
+        self.__is_plane_at_airport(plane)
         plane.land()
         self.planes.append(plane)
 
     def instruct_take_off(self, plane):
-        if plane not in self.planes:
-            raise Exception("Plane not at airport: land plane first")
+        self.__is_plane_not_at_airport(plane)
         plane.take_off()
         self.planes.remove(plane)
+
+    def __is_airport_full(self):
+        if len(self.planes) == self.capacity:
+            raise Exception('Airport is full: Take off plane')
+
+    def __is_plane_at_airport(self, plane):
+        if plane in self.planes:
+            raise Exception('Plane already at airport')
+
+    def __is_plane_not_at_airport(self, plane):
+        if plane not in self.planes:
+            raise Exception("Plane not at airport: land plane first")
