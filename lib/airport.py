d10618c3ed851ac2af6ee3ce29032a11efd23acb
Hanif Fakira <fakira.work@gmail.com>
2016-08-09 09:06:24 +0100

    injected weather class in init

--- lib/airport.py
+++ lib/airport.py
@@ -1,18 +1,18 @@
+from lib.weather import Weather
+
 class Airport(object):
-    def __init__(self, capacity = 20):
+    def __init__(self, weather = Weather(), capacity = 20):
         self.planes = []
+        self.weather = weather
         self.capacity = capacity
 
-    def instruct_to_land(self, plane, weather):
-        self.__check_weather_good_for_landing(weather)
-        self.__check_airport_full()
-        self.__check_plane_at_airport(plane)
+    def instruct_to_land(self, plane):
+        self.__check_for_landing(plane)
         plane.land()
         self.planes.append(plane)
 
-    def instruct_take_off(self, plane, weather):
-        self.__check_weather_good_for_taking_off(weather)
-        self.__check_plane_not_at_airport(plane)
+    def instruct_take_off(self, plane):
+        self.__check_for_take_off(plane)
         plane.take_off()
         self.planes.remove(plane)
 
@@ -24,14 +24,23 @@ class Airport(object):
         if plane in self.planes:
             raise Exception('Plane already at airport')
 
+    def __check_weather_good_for_landing(self):
+        if self.weather.is_stormy():
+            raise Exception('Plane cannot land: weather is stormy')
+
+    def __check_for_landing(self, plane):
+        self.__check_weather_good_for_landing()
+        self.__check_airport_full()
+        self.__check_plane_at_airport(plane)
+
     def __check_plane_not_at_airport(self, plane):
         if plane not in self.planes:
             raise Exception("Plane not at airport: land plane first")
 
-    def __check_weather_good_for_landing(self, weather):
-        if weather.is_stormy():
-            raise Exception('Plane cannot land: weather is stormy')
-
-    def __check_weather_good_for_taking_off(self, weather):
-        if weather.is_stormy():
+    def __check_weather_good_for_taking_off(self):
+        if self.weather.is_stormy():
             raise Exception('Plane cannot take off: weather is stormy')
+
+    def __check_for_take_off(self, plane):
+        self.__check_weather_good_for_taking_off()
+        self.__check_plane_not_at_airport(plane)
--- tests/lib_airport_tests.py
+++ tests/lib_airport_tests.py
@@ -5,10 +5,10 @@ from lib.airport import Airport
 class TestAirport(unittest.TestCase):
 
     def setUp(self):
-        self.airport = Airport()
         self.plane = MagicMock()
         self.weather = MagicMock()
         self.weather.is_stormy = MagicMock(return_value=False)
+        self.airport = Airport(self.weather)
 
     def test_0_defaults(self):
         """airport is empty"""
@@ -20,66 +20,66 @@ class TestAirport(unittest.TestCase):
 
     def test_0b_defaults(self):
         """airport can change capacity"""
-        self.airport = Airport(5)
+        self.airport = Airport(self.weather, 5)
         self.assertEqual(self.airport.capacity, 5)
 
     def test_1a_instruct_to_land(self):
         """plane in airport after landing"""
-        self.airport.instruct_to_land(self.plane, self.weather)
+        self.airport.instruct_to_land(self.plane)
         number_of_planes = self.airport.planes
         self.assertEqual(1, len(number_of_planes))
 
     def test_1b_instruct_to_land(self):
         """aiport hanger filled with one plane"""
-        self.airport.instruct_to_land(self.plane, self.weather)
+        self.airport.instruct_to_land(self.plane)
         self.assertIn(self.plane, self.airport.planes)
 
     def test_1c_instruct_to_land(self):
         """aiport instruct_to_land calls land on plane"""
-        self.airport.instruct_to_land(self.plane, self.weather)
+        self.airport.instruct_to_land(self.plane)
         self.plane.land.assert_called_with()
 
     def test_1d_instruct_to_land(self):
         """plane cannot land if already in airport"""
-        self.airport.instruct_to_land(self.plane, self.weather)
+        self.airport.instruct_to_land(self.plane)
         with self.assertRaisesRegexp(Exception, 'Plane already at airport'):
-            self.airport.instruct_to_land(self.plane, self.weather)
+            self.airport.instruct_to_land(self.plane)
 
     def test_1e_instruct_to_land(self):
         """plane cannot land if airport is full"""
         for number in range(1,21):
             self.diff_plane = MagicMock()
-            self.airport.instruct_to_land(self.diff_plane, self.weather)
+            self.airport.instruct_to_land(self.diff_plane)
         with self.assertRaisesRegexp(Exception, 'Airport is full: Take off plane'):
-            self.airport.instruct_to_land(self.plane, self.weather)
+            self.airport.instruct_to_land(self.plane)
 
     def test_1f_instruct_to_land(self):
         """plane cannot land if stormy weather"""
         self.weather.is_stormy = MagicMock(return_value=True)
         with self.assertRaisesRegexp(Exception, 'Plane cannot land: weather is stormy'):
-            self.airport.instruct_to_land(self.plane, self.weather)
+            self.airport.instruct_to_land(self.plane)
 
     def test_2a_instruct_take_off(self):
         """plane is not in airport after take off"""
-        self.airport.instruct_to_land(self.plane, self.weather)
-        self.airport.instruct_take_off(self.plane, self.weather)
+        self.airport.instruct_to_land(self.plane)
+        self.airport.instruct_take_off(self.plane)
         number_of_planes = self.airport.planes
         self.assertEqual(0, len(number_of_planes))
 
     def test_2b_instruct_take_off(self):
         """aiport instruct_take_off calls take_off on plane"""
-        self.airport.instruct_to_land(self.plane, self.weather)
-        self.airport.instruct_take_off(self.plane, self.weather)
+        self.airport.instruct_to_land(self.plane)
+        self.airport.instruct_take_off(self.plane)
         self.plane.take_off.assert_called_with()
 
     def test_2c_instruct_take_off(self):
         """plane cannot take off if not at airport"""
         with self.assertRaisesRegexp(Exception, 'Plane not at airport: land plane first'):
-            self.airport.instruct_take_off(self.plane, self.weather)
+            self.airport.instruct_take_off(self.plane)
 
     def test_2d_instruct_take_off(self):
         """plane cannot take off if stormy weather"""
-        self.airport.instruct_to_land(self.plane, self.weather)
+        self.airport.instruct_to_land(self.plane)
         self.weather.is_stormy = MagicMock(return_value=True)
         with self.assertRaisesRegexp(Exception, 'Plane cannot take off: weather is stormy'):
-            self.airport.instruct_take_off(self.plane, self.weather)
+            self.airport.instruct_take_off(self.plane)
